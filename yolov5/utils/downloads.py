137df35ec15bc423ef22992b4c0e822017e7dfe7
skystylez <prostyle@naver.com>
2021-12-28 09:32:49 +0900

    add yolov5 model

---
+++ yolov5/utils/downloads.py
@@ -0,0 +1,32 @@
+# YOLOv5 🚀 by Ultralytics, GPL-3.0 license
+"""
+Download utils
+"""
+
+import os
+import platform
+import time
+from pathlib import Path
+from zipfile import ZipFile
+
+import torch
+
+
+def gsutil_getsize(url=''):
+    return 0
+
+
+def safe_download(file, url, url2=None, min_bytes=1E0, error_msg=''):
+    return 0
+
+
+def attempt_download(file, repo=''):  # from utils.downloads import *; attempt_download()
+    return 0
+
+
+def gdrive_download(id='16TiPfZj7htmTyhntwcZyEEAejOUxuT6m', file='tmp.zip'):
+    return 0
+
+
+def get_token(cookie="./cookie"):
+    return ""
